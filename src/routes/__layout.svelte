0baa1144336a7cfaca7370715e83b5e5cd78e655
Tom <info@tomhoward.codes>
2021-10-30 10:25:54 -0400

    chore: disable footer

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -8,11 +8,11 @@
 	<slot />
 </div>
 
-<footer class="footer">
+<!-- <footer class="footer">
 	<div class="content has-text-centered">
 		<p>
 			<strong>Thor Fitness</strong> by <a href="https://www.tomhoward.codes/">Tom Howard</a>. Source
 			code can be found <a href="https://github.com/thoward27/thor">here</a>.
 		</p>
 	</div>
-</footer>
+</footer> -->
