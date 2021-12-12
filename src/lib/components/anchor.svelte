ef0770d32cb4027a9be1ea5b72dddb6ab9a10cd3
Tom <info@tomhoward.codes>
2021-12-12 09:27:57 -0500

    chore: add anchor component

---
+++ src/lib/components/anchor.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { Icon } from '$lib/components';
+	import { onMount } from 'svelte';
+
+	export let text: string | undefined = undefined;
+	export let icon: string | undefined = undefined;
+	export let color: 'primary' | 'info' | 'success' | 'warning' | 'danger' = 'primary';
+	export let href: string;
+
+	let disabled: boolean | undefined = true;
+	onMount(() => {
+		disabled = undefined;
+	});
+</script>
+
+<a {href} class="button is-{color}" {disabled}>
+	{#if icon}
+		<Icon {icon} />
+	{/if}
+	{#if text}
+		<span>
+			{text}
+		</span>
+	{/if}
+</a>
