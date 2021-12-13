2322e91b1251bbbdd109d09e590b850dd3dfeb35
Tom <info@tomhoward.codes>
2021-12-12 20:48:42 -0500

    export from gun

--- src/routes/data/export.svelte
+++ src/routes/data/export.svelte
@@ -1,15 +1,42 @@
 <script lang="ts">
 	import { Icon, Header } from '$lib/components';
-	import { workouts } from '$lib/stores';
 	import type { SetType } from '$lib/schemas';
+	import { onMount } from 'svelte';
 
-	// Filter out the stuff we don't need to save.
-	$: output = JSON.stringify(
-		$workouts.map((workout) => ({ ...workout, sets: cleanSets(workout.sets) })),
-		undefined,
-		2
-	);
+	const KEY = 'test-workouts/v1';
+	let store = {};
+	let gun;
+	let user;
+	onMount(async () => {
+		gun = (await import('$lib/initGun')).gun;
+		user = gun.user().recall({ sessionStorage: true });
+		gun
+			.get(KEY)
+			.get(user.is.pub)
+			.map()
+			.on(function (data, key) {
+				// TODO: https://gun.eco/docs/API#-a-name-open-a-gun-open-callback-
+				if (data) {
+					delete data['_'];
+					store[key] = { ...data, sets: [] };
+					gun
+						.get(KEY)
+						.get(user.is.pub)
+						.get(key)
+						.get('sets')
+						.map()
+						.once((data, _) => {
+							delete data['_']
+							store[key].sets = [...store[key].sets, data];
+						});
+				} else {
+					delete store[key];
+					store = store;
+				}
+			});
+	});
 
+	$: output = JSON.stringify(Object.entries(store), undefined, 2);
 	function cleanSets(sets: SetType[]): SetType[] {
 		return sets.filter((set) => !set.removed).map((set, index) => ({ ...set, id: index }));
 	}
