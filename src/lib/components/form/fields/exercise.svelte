f8e3f0de4f3791fabf70742f109e3413ebe69863
Tom <info@tomhoward.codes>
2022-01-03 11:55:00 -0500

    WIP

---
+++ src/lib/components/form/fields/exercise.svelte
@@ -0,0 +1,27 @@
+<script context="module">
+    let counter = 0;
+</script>
+<script>
+    import { onMount } from "svelte";
+
+    let gun;
+    onMount(async () => {
+        gun = (await import('$lib/initGun')).gun;
+    });
+
+    let id = `autocompleteExercise${counter}`;
+</script>
+
+<svelte:head>
+	<link
+		rel="stylesheet"
+		href="https://cdn.jsdelivr.net/npm/@tarekraafat/autocomplete.js@10.2.6/dist/css/autoComplete.02.min.css"
+	/>
+</svelte:head>
+
+<div class="control">
+    <label>
+        Exercise
+
+    </label>
+</div>
